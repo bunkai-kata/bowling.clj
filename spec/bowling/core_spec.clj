9768400dc156e2915a738b554dcebeda8b7b1e0a
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-17 13:27:49 -0500

    1

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -3,9 +3,17 @@
             [speclj.core :refer :all]))
 
 (describe "scoring a bowling game"
+
   (it "scores a gutter game"
     (let [g (new-game)]
       (should= 0
                (->> (repeat 20 0)
+                    (reduce #(roll %1 %2) g)
+                    score))))
+
+  (it "scores a singles game"
+    (let [g (new-game)]
+      (should= 20
+               (->> (repeat 20 1)
                     (reduce #(roll %1 %2) g)
                     score)))))
