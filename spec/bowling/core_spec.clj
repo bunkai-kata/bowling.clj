7b37710e5fa2f8d16bef7dee07803e5ffae24ddd
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-17 10:24:53 -0500

    1

--- spec/bowling/core_spec.clj
+++ spec/bowling/core_spec.clj
@@ -1,3 +1,7 @@
 (ns bowling.core-spec
   (:require [bowling.core :refer :all]
             [speclj.core :refer :all]))
+
+(describe "Scoring a bowling game"
+  (it "scores a gutter game"
+    (let [game (new-game)])))
