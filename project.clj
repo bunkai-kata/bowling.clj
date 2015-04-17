da5a933712d5f2a55a0383c240a55b15ee4fab1b
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-17 10:00:19 -0500

    Add speclj to the project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,14 @@
   :description "The bowling kata, done in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :test-paths ["spec"]
+
   :main ^:skip-aot bowling.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]]
+                   :plugins      [[speclj "3.2.0"]]}
+             :uberjar {:aot :all}})
